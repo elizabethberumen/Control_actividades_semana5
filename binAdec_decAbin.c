637d15d40631fef4f31b4ba0fa3df401bee53ec1
CORP\eberumen <elizabeth.berumen@capgemini.com>
2023-01-25 12:50:59 -0600

    se agrego el programa de decimal a binario

--- binAdec_decAbin.c
+++ binAdec_decAbin.c
@@ -25,6 +25,27 @@ int binario_decima(int temp, int numbin){
 	
 	return dec;
 }
+	
+
+void dec_bin(int n){
+	
+	int binarray[33];
+	int i=0;
+	
+	while(n>0){
+		
+		binarray[i]=n%2;
+		n=n/2;
+		i++;
+			
+	}
+	
+	for(int j=i-1; j>=0; j--){
+		printf("Este es el numero en binario: %d\n", binarray[j]);
+		
+	}
+	
+}
 
 
 int main(int argc, char *argv[]) {
@@ -35,6 +56,12 @@ int main(int argc, char *argv[]) {
 	temp=numbin;//aqui se guarda el numero binario en una variable temporal
 	
 	binario_decima(temp, numbin);
+	
+	
+	//dec a binario
+	
+	int n=10;
+	dec_bin(n);
 	
 	return 0;
 }
